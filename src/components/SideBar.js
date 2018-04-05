7d4ed78811ed57bd8b92009e0c4c503236ea50f7
ZachMoore <Get3Moore@gmail.com>
2018-04-05 17:02:59 -0400

    add better tab focus

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -81,11 +81,11 @@ class SideBar extends Component {
         return (<nav className={this.props.navIsShown ? this.styles.open : this.styles.closed}>
 
             <h1 className="text-center nav-title">Neighborhood Restaurants</h1>
-            <section className="flex-container column full-width">
-                <label htmlFor="searchBox">Search Restaruants:</label>
-                <input type="text" id="searchBox" placeholder="e.g. Texas Hot" onChange={this.updateValue} value={this.state.searchValue} aria-label="restaurant search field"/>
+            <section className="flex-container column flex-column-left full-width search-box-container">
+                <label htmlFor="searchBox" className="search-box-label">Search Restaruants:</label>
+                <input type="text" id="searchBox" placeholder="e.g. Texas Hot" className="search-box" onChange={this.updateValue} value={this.state.searchValue} aria-label="restaurant search field"/>
             </section>
-            <ul className="restaurants-list" aria-label="restaurant search results">
+            <ul className="restaurants-list flex-container column flex-column-left full-width" aria-label="restaurant search results">
                 {this.state.filtered.map(object => <ListItem name={object.restaurant.name} key={object.restaurant.id} id={object.restaurant.id} updateSelection={this.updateSelection}/>)}
             </ul>
             
@@ -94,8 +94,8 @@ class SideBar extends Component {
 };
 
 const ListItem = (props) => {
-    return (<li>
-        <a href="#item" onClick={()=>{
+    return (<li className="restaurant-li">
+        <a className="restaurant-link" href="#item" onClick={()=>{
             props.updateSelection(props.id)
         }}>{props.name}</a>
     </li>)
