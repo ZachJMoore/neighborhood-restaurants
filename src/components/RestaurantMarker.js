f076fd5fc1ae49507c656a558476c400fdb4e5bd
Zach Moore <get3moore@gmail.com>
2019-10-19 22:06:06 -0400

    Update styling and deps

--- src/components/RestaurantMarker.js
+++ src/components/RestaurantMarker.js
@@ -1,83 +1,127 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 class RestaurantMarker extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
-
-        this.state = {
-            isShown: false,
-            override: false
-        };
-
-        this.styles = {
-            open: "restaurant-details flex-container column details-shown",
-            closed: "restaurant-details flex-container column",
-            marker: "marker",
-            markerActive: "marker marker-active"};
-
-        this.toggleStyles = () => {
-            if (this.state.isShown){
-                this.setState({isShown: false})
-                if (this.props.restReferrer !== null){
-                    this.props.restReferrer.focus() //Tab focus redirect: focus on link that sent someone here when item is closed. 
-                }
-            } else {
-                this.setState({isShown: true})
-            }
-            this.setState({override: true})
-        };
-
+    this.state = {
+      isShown: false,
+      override: false
     };
 
-    componentDidUpdate(){
-        //logic to show or hide a marker. Specifically when an incoming restaurant value has a isShonw value set to true
-        //if isShown is not already true (or false), and the user hasnt overridden it by manually closing the marker, change the isShown value
-        if (!this.state.isShown && !this.state.override && this.props.isShown){
-            this.setState({isShown: true});
-        } else if (this.state.isShown && !this.props.isShown && !this.state.override){
-            this.setState({isShown: false});
-        };
+    this.styles = {
+      open: "restaurant-details flex-container column details-shown",
+      closed: "restaurant-details flex-container column",
+      marker: "marker",
+      markerActive: "marker marker-active"
     };
 
-    componentWillReceiveProps(){
-        //reset override value if the marker is hidden
-        if (!this.state.isShown && this.state.override){
-            this.setState({override: false});
-        };
+    this.toggleStyles = () => {
+      if (this.state.isShown) {
+        this.setState({ isShown: false });
+        if (this.props.restReferrer !== null) {
+          this.props.restReferrer.focus(); //Tab focus redirect: focus on link that sent someone here when item is closed.
+        }
+      } else {
+        this.setState({ isShown: true });
+      }
+      this.setState({ override: true });
     };
+  }
 
-    render(){
-        return (<section className="restaurant-details-container" {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})}>
+  componentDidUpdate() {
+    //logic to show or hide a marker. Specifically when an incoming restaurant value has a isShonw value set to true
+    //if isShown is not already true (or false), and the user hasnt overridden it by manually closing the marker, change the isShown value
+    if (!this.state.isShown && !this.state.override && this.props.isShown) {
+      this.setState({ isShown: true });
+    } else if (
+      this.state.isShown &&
+      !this.props.isShown &&
+      !this.state.override
+    ) {
+      this.setState({ isShown: false });
+    }
+  }
 
-            {/* marker icon */}
-            <a ref={"marker"} href="#open" {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})} className={this.state.isShown ? this.styles.markerActive : this.styles.marker} onClick={this.toggleStyles}>{this.props.restaurant.name + " restaurant marker"}</a>
+  UNSAFE_componentWillReceiveProps() {
+    //reset override value if the marker is hidden
+    if (!this.state.isShown && this.state.override) {
+      this.setState({ override: false });
+    }
+  }
 
-            {/* marker details section. */}
-            {/* main functionality here: set styles for hidden or shown based on the restaurant isShown value */}
-            <section className={this.state.isShown ? this.styles.open : this.styles.closed}>
-                <h2 {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})}>{this.props.restaurant.name}</h2>
-                <ul aria-label="searched restaurant list">
-                    <li {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})}>Average Cost for Two: {this.props.restaurant.currency}{this.props.restaurant.average_cost_for_two}</li>
-                    <li {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})}>Cuisines: {this.props.restaurant.cuisines}</li>
-                </ul>
-                <p>For more information click <a href={this.props.restaurant.url} {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})} target="_blank" aria-label="more restaurant information">here</a>.</p>
-                <small>Data provided by Zomato</small>
-                <a href="#close" {...(this.state.isShown ? {tabIndex: 0} : {tabIndex: -1})} className="close" onClick={this.toggleStyles} aria-label="close details">×</a>
-            </section>
+  render() {
+    return (
+      <section
+        className="restaurant-details-container"
+        {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}
+      >
+        {/* marker icon */}
+        <a
+          ref={"marker"}
+          href="#open"
+          {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}
+          className={
+            this.state.isShown ? this.styles.markerActive : this.styles.marker
+          }
+          onClick={this.toggleStyles}
+        >
+          {this.props.restaurant.name + " restaurant marker"}
+        </a>
+
+        {/* marker details section. */}
+        {/* main functionality here: set styles for hidden or shown based on the restaurant isShown value */}
+        <section
+          className={this.state.isShown ? this.styles.open : this.styles.closed}
+        >
+          <h2 {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}>
+            {this.props.restaurant.name}
+          </h2>
+          <ul aria-label="searched restaurant list">
+            <li {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}>
+              Average Cost for Two: {this.props.restaurant.currency}
+              {this.props.restaurant.average_cost_for_two}
+            </li>
+            <li {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}>
+              Cuisines: {this.props.restaurant.cuisines}
+            </li>
+          </ul>
+          <p>
+            For more information click{" "}
+            <a
+              href={this.props.restaurant.url}
+              {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="more restaurant information"
+            >
+              here
+            </a>
+            .
+          </p>
+          <small>Data provided by Zomato</small>
+          <a
+            href="#close"
+            {...(this.state.isShown ? { tabIndex: 0 } : { tabIndex: -1 })}
+            className="close"
+            onClick={this.toggleStyles}
+            aria-label="close details"
+          >
+            ×
+          </a>
+        </section>
 
         {/* Manage focus */}
-        {(()=>{
-                if (this.state.isShown){
-                    this.refs.marker.focus();
-                };
-            })()}
+        {(() => {
+          if (this.state.isShown) {
+            this.refs.marker.focus();
+          }
+        })()}
+      </section>
+    );
+  }
+}
 
-        </section>)
-        
-    };
-};
-
-export default RestaurantMarker
+export default RestaurantMarker;
 
 // {...(condition ? {bsStyle: 'success'} : {})}
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,103 +3,140 @@ import escapeRegExp from "escape-string-regexp";
 import { placeholderRestaurants } from "../static/placeholderRestaurant";
 
 class SideBar extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
-
-        this.state = {
-            searchValue: "",
-            filtered: [],
-            selectionId: ""
-        };
-
-        this.updateValue = (event) => {
-            this.setState({searchValue: event.target.value, selectionId: ""}, this.filterRestaurants)
-        };
-
-        this.filterRestaurants = () => {
-            const match = new RegExp(escapeRegExp(this.state.searchValue), "i");
-            let filtered = [];
-
-            //filter based on RegExp
-            if (this.state.searchValue !== "") {
-                filtered = this.props.restaurants.filter((object) => match.test(object.restaurant.name));
-            } else {
-                filtered = this.props.restaurants;
-            };
-
-            //map over and set isShown value. If a item has been selected set isShown to true, otherwise set to false;
-            filtered = filtered.map(object => {
-                if (object.restaurant.id === this.state.selectionId) {
-                    let newObject = object;
-                    newObject.restaurant.isShown = true;
-                    
-                    return newObject;
-                } else {
-                    let newObject = object;
-                    newObject.restaurant.isShown = false;
-                    
-                    return newObject;
-                }
-                
-            });
-
-            //if no results, return a placeholder
-            if (filtered.length === 0){
-                filtered = placeholderRestaurants;
-            };
-            
-            //then set filtered state to use for the list.
-            this.setState({filtered: filtered});
-            //update filtered object property in app.js state to pass down to map
-            this.props.updateFiltered(filtered);
-        };
-
-        //function to update the selected restaurant, then filter restaurants again
-        this.updateSelection = (id, ref) => {
-            this.setState({selectionId: id}, this.filterRestaurants);
-            this.props.updateReferrer(ref) //update app referer for sending tab focus back once marker is closed
-        };
-
-        this.styles = {open: "nav nav-shown", closed: "nav"};
+    this.state = {
+      searchValue: "",
+      filtered: [],
+      selectionId: ""
     };
 
-
-    componentWillReceiveProps(){
-        //if no filtered results run filterRestaruants. This keeps the listed items up to date and on first page load
-        if (!this.state.filtered.length){
-            this.filterRestaurants();
-        } else if (this.state.filtered[0].restaurant.placeholder && this.state.searchValue.length === 0 && !this.props.networkError){
-            //if the showing restaurant is a placeholder, and there is no search result, and it is not because of a network error, fitler restaurants again to update things.
-            //this is needed because of initial state when waiting for ajax request to come back and setState.
-            //an empty array is passed until that happens. this also helps if there is a ajax error and it will then just display a placeholder on the map
-            this.filterRestaurants();
-        };
+    this.updateValue = event => {
+      this.setState(
+        { searchValue: event.target.value, selectionId: "" },
+        this.filterRestaurants
+      );
     };
 
-    render(){
-        return (<nav className={this.props.navIsShown ? this.styles.open : this.styles.closed}>
-            <h1 className="text-center nav-title" >Neighborhood Restaurants</h1>
-            <section className="flex-container column full-width search-box-container">
-                <label htmlFor="searchBox" className="search-box-label" >Search Restaurants:</label>
-                <input type="text" id="searchBox" placeholder="e.g. Texas Hot" className="search-box" onChange={this.updateValue} value={this.state.searchValue} aria-label="restaurant search field"/>
-            </section>
-            <ul className="restaurants-list" aria-label="restaurant search results">
-                {this.state.filtered.map(object => <ListItem name={object.restaurant.name} key={object.restaurant.id} id={object.restaurant.id} updateSelection={this.updateSelection}/>)}
-            </ul>
+    this.filterRestaurants = () => {
+      const match = new RegExp(escapeRegExp(this.state.searchValue), "i");
+      let filtered = [];
 
-        </nav>)
+      //filter based on RegExp
+      if (this.state.searchValue !== "") {
+        filtered = this.props.restaurants.filter(object =>
+          match.test(object.restaurant.name)
+        );
+      } else {
+        filtered = this.props.restaurants;
+      }
+
+      //map over and set isShown value. If a item has been selected set isShown to true, otherwise set to false;
+      filtered = filtered.map(object => {
+        if (object.restaurant.id === this.state.selectionId) {
+          let newObject = object;
+          newObject.restaurant.isShown = true;
+
+          return newObject;
+        } else {
+          let newObject = object;
+          newObject.restaurant.isShown = false;
+
+          return newObject;
+        }
+      });
+
+      //if no results, return a placeholder
+      if (filtered.length === 0) {
+        filtered = placeholderRestaurants;
+      }
+
+      //then set filtered state to use for the list.
+      this.setState({ filtered: filtered });
+      //update filtered object property in app.js state to pass down to map
+      this.props.updateFiltered(filtered);
     };
-};
+
+    //function to update the selected restaurant, then filter restaurants again
+    this.updateSelection = (id, ref) => {
+      this.setState({ selectionId: id }, this.filterRestaurants);
+      this.props.updateReferrer(ref); //update app referer for sending tab focus back once marker is closed
+    };
+
+    this.styles = { open: "nav nav-shown", closed: "nav" };
+  }
+
+  UNSAFE_componentWillReceiveProps() {
+    //if no filtered results run filterRestaruants. This keeps the listed items up to date and on first page load
+    if (!this.state.filtered.length) {
+      this.filterRestaurants();
+    } else if (
+      this.state.filtered[0].restaurant.placeholder &&
+      this.state.searchValue.length === 0 &&
+      !this.props.networkError
+    ) {
+      //if the showing restaurant is a placeholder, and there is no search result, and it is not because of a network error, fitler restaurants again to update things.
+      //this is needed because of initial state when waiting for ajax request to come back and setState.
+      //an empty array is passed until that happens. this also helps if there is a ajax error and it will then just display a placeholder on the map
+      this.filterRestaurants();
+    }
+  }
+
+  render() {
+    return (
+      <nav
+        className={
+          this.props.navIsShown ? this.styles.open : this.styles.closed
+        }
+      >
+        <h1 className="text-center nav-title">Neighborhood Restaurants</h1>
+        <section className="flex-container column full-width search-box-container">
+          <label htmlFor="searchBox" className="search-box-label">
+            Search Restaurants:
+          </label>
+          <input
+            type="text"
+            id="searchBox"
+            placeholder="e.g. Texas Hot"
+            className="search-box"
+            onChange={this.updateValue}
+            value={this.state.searchValue}
+            aria-label="restaurant search field"
+          />
+        </section>
+        <ul className="restaurants-list" aria-label="restaurant search results">
+          {this.state.filtered.map(object => (
+            <ListItem
+              name={object.restaurant.name}
+              key={object.restaurant.id}
+              id={object.restaurant.id}
+              updateSelection={this.updateSelection}
+            />
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+}
 
 class ListItem extends Component {
-    render(){
-        return (<li className="restaurant-li flex-container">
-        <a ref="link" className="restaurant-link" href="#item" onClick={()=>{
-            this.props.updateSelection(this.props.id, this.refs.link)
-        }}>{this.props.name}</a>
-    </li>)
-    };
-};
+  render() {
+    return (
+      <li className="restaurant-li flex-container">
+        <a
+          ref="link"
+          className="restaurant-link"
+          href="#item"
+          onClick={() => {
+            this.props.updateSelection(this.props.id, this.refs.link);
+          }}
+        >
+          {this.props.name}
+        </a>
+      </li>
+    );
+  }
+}
 
-export default SideBar
+export default SideBar;
